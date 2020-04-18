f33e661e775d7032918869674716aa3279b32424
Yi <yiy68@pitt.edu>
2020-04-18 01:04:42 -0400

    change config file

--- object_flow/nn/yolov3_tf2/yolotf2.py
+++ object_flow/nn/yolov3_tf2/yolotf2.py
@@ -21,7 +21,8 @@ import numpy as np
 import cv2
 
 from object_flow.ipc.doer import Doer
-
+import tensorflow as tf
+from object_flow.nn.yolov3_tf2.models import YoloV3
 from object_flow.nn.yolov3_tf2.dataset import transform_images
 from object_flow.util.mmap_frames import MmapFrames
 from object_flow.util.mmap_bboxes import MmapBboxes
@@ -35,8 +36,6 @@ class YoloTf2(Doer):
     # ---------------------------------------------------------------------------------
 
     def __init__(self):
-        import tensorflow as tf
-        from object_flow.nn.yolov3_tf2.models import YoloV3
         super().__init__()
         
         logging.info("yolotf2 initialization started")
@@ -123,7 +122,7 @@ class YoloTf2(Doer):
     # ---------------------------------------------------------------------------------
 
     def find_bboxes(self, video_name, frame_index):
-        import tensorflow as tf
+
         frame_number, frame = self.videos[video_name]['frames'].read_data(frame_index)
         video_id = self.videos[video_name]['video_id']
         width = self.videos[video_name]['width']
