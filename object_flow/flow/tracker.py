0f3d3a3b8ff02a47c83ee205c6da6da1919d9cde
Yi <yiy68@pitt.edu>
2020-04-19 00:09:00 -0400

    add logging

--- object_flow/flow/tracker.py
+++ object_flow/flow/tracker.py
@@ -167,7 +167,7 @@ class Tracker(Doer):
             detections[item_id] = (confidence, pos)
             end_time = time.perf_counter()
             if end_time-start_time>0.018:
-                logging.info("find an object %s that is taking too long at frame %s", item_id, frame_index)
+                logging.info("Tracker %s find an object %s that is taking too long at frame %s", self.id, item_id, frame_index)
             # logging.info("Tracker Id: %s, Video name: %s, Item id: %d, Time took: %f",str(self.id), video_name, item_id, end_time-start_time)
 
         Stopwatch.stop('update_tracking') 
